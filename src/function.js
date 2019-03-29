a59f84a253d5483a654856fddcf3f097a3f9bdf8
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-29 11:31:41 +0100

    Impleneted some of the tested funtions such as decryption encryption and other sub methods

--- src/function.js
+++ src/function.js
@@ -33,3 +33,57 @@ exports.anaylze = function(arrayOfNumbers){
     length
   }
 }
+
+
+exports.decryption = function (word, step) {
+  const capitals = generateUpperCaseLetters();
+  const lowCapitals = generateLowerCaseLettes()
+
+  const wordsAsArray = word.split('');
+  const encryptedWordsOfArray = []
+  wordsAsArray.forEach((item) => {
+    if (isCharUpperCase(item)) {
+      encryptedWordsOfArray.push(getDecryptCeaserChar(capitals, item, step))
+    }
+    else {
+      encryptedWordsOfArray.push(getDecryptCeaserChar(lowCapitals, item, step))
+    }
+  })
+
+  return encryptedWordsOfArray.join('')
+}
+
+
+exports.encryption = function (word, step) {
+  const capitals = generateUpperCaseLetters();
+  const lowCapitals = generateLowerCaseLettes()
+
+  const wordsAsArray = word.split('');
+  const encryptedWordsOfArray = []
+  wordsAsArray.forEach((item) => {
+    if (isCharUpperCase(item)) {
+      encryptedWordsOfArray.push(getCeaserChar(capitals, item, step))
+    }
+    else {
+      encryptedWordsOfArray.push(getCeaserChar(lowCapitals, item, step))
+    }
+  })
+
+  return encryptedWordsOfArray.join('')
+}
+
+function generateLowerCaseLettes() {
+  const lowCapitals = []
+  for(let i=10; i < 36; i++){
+    lowCapitals.push(i.toString(36))
+  }
+  return lowCapitals
+}
+
+function generateUpperCaseLetters() {
+  const capitals = [];
+  for(let i=10; i < 36; i++){
+    capitals.push(i.toString(36).toUpperCase())
+  }
+  return capitals
+}
