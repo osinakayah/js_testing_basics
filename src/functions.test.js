f1b85c549495df9a75f873a0c7314a3735df9376
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-29 11:13:59 +0100

    Added test for the functions to implement

--- src/functions.test.js
+++ src/functions.test.js
@@ -14,3 +14,23 @@ test("Test calculator object sum method", () => {
 test("Test calculator object subtract method", () => {
   expect(calculator.subtract(1,2)).toBe(-1)
 })
+
+test("Test calculator object divide method", () => {
+  expect(calculator.divide(1,2)).toBe(0.5)
+})
+
+test("Test calculator object multiply method", () => {
+  expect(calculator.multiply(1,2)).toBe(2)
+})
+
+test("Test Caser encryption algorithm", () => {
+  expect(encryption("attack at dawn", 1)).toBe("buubdl bu ebxo")
+})
+
+test("Test Caser decryption algorithm", () => {
+  expect(decryption("buubdl bu ebxo", 1)).toBe("attack at dawn")
+})
+
+test("Test Analyze array", () => {
+  expect(anaylze([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6})
+})
